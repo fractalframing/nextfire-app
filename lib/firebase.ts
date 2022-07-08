52c08235d4afc5bd51b3653a0aae3f84b6996a12
Fractal Framing <55963511+fractalframing@users.noreply.github.com>
2022-07-07 21:24:31 -0600

    v 3

---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+import config  from '../config/firebaseconfig.json';
+
+const firebaseConfig = config;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
